b5289c9fdce567b94c4f97e0581b1955efb0b206
marco <z405656232x@163.com>
2017-03-05 02:09:07 +0800

    more common entities added

---
+++ uniform-cms-biz/src/main/java/com/nokia/ucms/biz/constants/Constants.java
@@ -0,0 +1,9 @@
+package com.nokia.ucms.biz.constants;
+
+/**
+ * Created by x36zhao on 2017/3/5.
+ */
+public final class Constants
+{
+    public static final String DYNAMIC_TABLE_PREFIX = "d_";
+}
---
+++ uniform-cms-biz/src/main/java/com/nokia/ucms/biz/entity/Role.java
@@ -0,0 +1,10 @@
+package com.nokia.ucms.biz.entity;
+
+import com.nokia.ucms.common.entity.BaseEntity;
+
+/**
+ * Created by x36zhao on 2017/3/5.
+ */
+public class Role extends BaseEntity
+{
+}
